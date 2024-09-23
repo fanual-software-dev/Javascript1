63899c8ad74ba00395006912da23b3b8afebaa6a
Fanual Asfaw <106735395+fanual-software-dev@users.noreply.github.com>
2024-09-23 18:43:31 +0300

    Add files via upload

---
+++ stringConcat.js
@@ -0,0 +1,5 @@
+var userInput = 'New to'
+
+var newString = userInput + 'Javascript'
+
+console.log(newString)
---
+++ triangle.js
@@ -0,0 +1,7 @@
+var side1 = 5;
+var side2 = 6;
+var side3 = 7;
+
+var perimeter = side1 + side2 + side3;
+
+console.log(perimeter);
